3546b879123f8f94aed9c5378172134d9535c1bc
Gregory Sheygam <sheygam@gmail.com>
2018-08-21 11:01:48 +0300

    test http get request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CityModel} from './city.model';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+    cities: CityModel[] = [];
+
+    constructor(private httpClient: HttpClient){}
+
+    ngOnInit(){
+      let url = 'https://olimshelper.herokuapp.com/';
+
+      this.httpClient.get<CityModel[]>(url + '/en/city')
+        .subscribe((response: CityModel[]) => this.cities = response);
+    }
 }
---
+++ src/app/city.model.ts
@@ -0,0 +1,6 @@
+export class CityModel {
+  name: string;
+  placeId: string;
+  latitude: number;
+  longitude: number;
+}
